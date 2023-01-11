9f5114a97b08fddf70231457c99e51be6b251ac7
Philippe Gaultier <philippe.gaultier@ppro.com>
2023-01-11 13:25:29 +0100

    ui-wip

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,228 @@
+use iced::alignment::{self, Alignment};
+use iced::theme::{self, Theme};
+use iced::widget::Column;
+use iced::widget::{
+    self, button, checkbox, column, container, row, scrollable, text, text_input, Text,
+};
+use iced::window;
+use iced::{Application, Element};
+use iced::{Color, Command, Font, Length, Settings, Subscription};
 use img_hash::HasherConfig;
-use std::{ffi::OsStr, process::Stdio};
+use std::{ffi::OsStr, path::PathBuf, process::Stdio};
 use walkdir::WalkDir;
 
-fn main() {
-    let arg1 = std::env::args().nth(1);
-    let root = arg1
-        .as_ref()
-        .map(|s| s.as_str())
-        .unwrap_or("/Users/pgaultier/Downloads");
+#[derive(Debug)]
+enum Ui {
+    Loading,
+    Loaded(UiState),
+}
 
-    let hasher = HasherConfig::new()
-        .hash_size(16, 16)
-        .hash_alg(img_hash::HashAlg::DoubleGradient)
-        .to_hasher();
-    let known_extensions = [
-        "png", "jpg", "jpeg", "gif", "bmp", "ico", "tiff", "webp", "avif", "pnm", "dds", "tga",
-    ]
-    .map(OsStr::new);
+#[derive(Debug)]
+struct UiState {
+    root: PathBuf,
+}
 
-    let mut path_hashes = Vec::with_capacity(100);
+#[derive(Debug, Clone)]
+pub struct Image {
+    path: PathBuf,
+    hash: img_hash::ImageHash,
+}
 
-    for entry in WalkDir::new(root) {
-        let entry = entry.unwrap();
-        if !entry.file_type().is_file() {
-            continue;
-        }
-        let ext = entry.path().extension();
-        if ext.is_none() {
-            continue;
-        }
-        let ext = ext.unwrap();
+#[derive(Debug, Clone)]
+pub enum UiMessage {
+    RootSelected(PathBuf),
+    HashComputed(Image),
+    SimilarityFound(Image, Image),
+}
 
-        if known_extensions.iter().find(|x| *x == &ext).is_none() {
-            continue;
-        }
-        println!("{}", entry.path().display());
-
-        let img = image::open(entry.path());
-        if let Err(err) = img {
-            eprintln!("Failed to open {:?}: {}", entry.path(), err);
-            continue;
-        }
-        let img = img.unwrap();
-
-        let hash = hasher.hash_image(&img);
-
-        path_hashes.push((hash, entry.path().to_owned()));
+impl UiState {
+    pub fn view(&self) -> Column<UiMessage> {
+        // We use a column: a simple vertical layout
+        column![
+            button("Image folder").on_press(UiMessage::RootSelected(PathBuf::from(
+                "/Users/pgaultier/Downloads"
+            ))),
+            text(self.root.display()),
+        ]
     }
 
-    let mut similar_count = 0usize;
-    let similarity_threshold = 20;
-    for (i, (a_hash, a_path)) in path_hashes.iter().enumerate() {
-        for j in 0..i {
-            let (b_hash, b_path) = &path_hashes[j];
-            assert_ne!(a_path, b_path);
+    pub fn update(&mut self, message: UiMessage) {
+        match message {
+            UiMessage::RootSelected(_root) => {}
+            UiMessage::HashComputed(_img) => {}
+            UiMessage::SimilarityFound(_a, _b) => {}
+        }
+    }
+}
 
-            if a_hash.dist(b_hash) <= similarity_threshold {
-                println!(
-                    "{} and {} might be similar",
-                    a_path.display(),
-                    b_path.display(),
-                );
-                similar_count += 1;
+impl Application for Ui {
+    type Message = UiMessage;
+    type Theme = Theme;
+    type Executor = iced::executor::Default;
+    type Flags = ();
 
-                let cmd = std::process::Command::new("open")
-                    .args([a_path, b_path])
-                    .stdout(Stdio::null())
-                    .stderr(Stdio::null())
-                    .spawn();
-                if let Err(err) = cmd {
-                    eprintln!(
-                        "Failed to run open: {} {} {}",
-                        err,
-                        a_path.display(),
-                        b_path.display()
-                    );
-                    continue;
-                }
-                if let Err(err) = cmd.unwrap().wait() {
-                    eprintln!(
-                        "Failed to wait for open: {} {} {}",
-                        err,
-                        a_path.display(),
-                        b_path.display()
-                    );
-                    continue;
-                }
+    fn new(_flags: ()) -> (Ui, Command<Self::Message>) {
+        (Ui::Loading, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Image dedup")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Self::Message> {
+        match self {
+            Ui::Loading => loading_message(),
+            Ui::Loaded(state) => {
+                let title = text("todos")
+                    .width(Length::Fill)
+                    .size(100)
+                    .style(Color::from([0.5, 0.5, 0.5]))
+                    .horizontal_alignment(alignment::Horizontal::Center);
+
+                let content = column![title].spacing(20).max_width(800);
+
+                scrollable(
+                    container(content)
+                        .width(Length::Fill)
+                        .padding(40)
+                        .center_x(),
+                )
+                .into()
             }
         }
     }
-    let total = path_hashes.len() * (path_hashes.len() - 1) / 2;
-    println!(
-        "Analyzed: {}, similar: {}/{}",
-        path_hashes.len(),
-        similar_count,
-        total
-    );
+}
+
+fn loading_message<'a>() -> Element<'a, UiMessage> {
+    container(
+        text("Loading...")
+            .horizontal_alignment(alignment::Horizontal::Center)
+            .size(50),
+    )
+    .width(Length::Fill)
+    .height(Length::Fill)
+    .center_y()
+    .into()
+}
+
+fn empty_message(message: &str) -> Element<'_, UiMessage> {
+    container(
+        text(message)
+            .width(Length::Fill)
+            .size(25)
+            .horizontal_alignment(alignment::Horizontal::Center)
+            .style(Color::from([0.7, 0.7, 0.7])),
+    )
+    .width(Length::Fill)
+    .height(Length::Units(200))
+    .center_y()
+    .into()
+}
+
+fn main() -> iced::Result {
+    Ui::run(Settings {
+        window: window::Settings {
+            size: (500, 800),
+            ..window::Settings::default()
+        },
+        ..Settings::default()
+    })
+
+    // let arg1 = std::env::args().nth(1);
+    // let root = arg1
+    //     .as_ref()
+    //     .map(|s| s.as_str())
+    //     .unwrap_or("/Users/pgaultier/Downloads");
+
+    // let hasher = HasherConfig::new()
+    //     .hash_size(16, 16)
+    //     .hash_alg(img_hash::HashAlg::DoubleGradient)
+    //     .to_hasher();
+    // let known_extensions = [
+    //     "png", "jpg", "jpeg", "gif", "bmp", "ico", "tiff", "webp", "avif", "pnm", "dds", "tga",
+    // ]
+    // .map(OsStr::new);
+
+    // let mut path_hashes = Vec::with_capacity(100);
+
+    // for entry in WalkDir::new(root) {
+    //     let entry = entry.unwrap();
+    //     if !entry.file_type().is_file() {
+    //         continue;
+    //     }
+    //     let ext = entry.path().extension();
+    //     if ext.is_none() {
+    //         continue;
+    //     }
+    //     let ext = ext.unwrap();
+
+    //     if known_extensions.iter().find(|x| *x == &ext).is_none() {
+    //         continue;
+    //     }
+    //     println!("{}", entry.path().display());
+
+    //     let img = image::open(entry.path());
+    //     if let Err(err) = img {
+    //         eprintln!("Failed to open {:?}: {}", entry.path(), err);
+    //         continue;
+    //     }
+    //     let img = img.unwrap();
+
+    //     let hash = hasher.hash_image(&img);
+
+    //     path_hashes.push((hash, entry.path().to_owned()));
+    // }
+
+    // let mut similar_count = 0usize;
+    // let similarity_threshold = 20;
+    // for (i, (a_hash, a_path)) in path_hashes.iter().enumerate() {
+    //     for j in 0..i {
+    //         let (b_hash, b_path) = &path_hashes[j];
+    //         assert_ne!(a_path, b_path);
+
+    //         if a_hash.dist(b_hash) <= similarity_threshold {
+    //             println!(
+    //                 "{} and {} might be similar",
+    //                 a_path.display(),
+    //                 b_path.display(),
+    //             );
+    //             similar_count += 1;
+
+    //             let cmd = std::process::Command::new("open")
+    //                 .args([a_path, b_path])
+    //                 .stdout(Stdio::null())
+    //                 .stderr(Stdio::null())
+    //                 .spawn();
+    //             if let Err(err) = cmd {
+    //                 eprintln!(
+    //                     "Failed to run open: {} {} {}",
+    //                     err,
+    //                     a_path.display(),
+    //                     b_path.display()
+    //                 );
+    //                 continue;
+    //             }
+    //             if let Err(err) = cmd.unwrap().wait() {
+    //                 eprintln!(
+    //                     "Failed to wait for open: {} {} {}",
+    //                     err,
+    //                     a_path.display(),
+    //                     b_path.display()
+    //                 );
+    //                 continue;
+    //             }
+    //         }
+    //     }
+    // }
+    // let total = path_hashes.len() * (path_hashes.len() - 1) / 2;
+    // println!(
+    //     "Analyzed: {}, similar: {}/{}",
+    //     path_hashes.len(),
+    //     similar_count,
+    //     total
+    // );
 }
