51850af2a753cca920c21030f899898363e5a832
Philippe Gaultier <philippe.gaultier@ppro.com>
2023-01-11 09:57:20 +0100

    [wip]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use image::io::Reader as ImageReader;
+use image::{ Luma};
 use std::ffi::OsStr;
 use std::path::PathBuf;
 use walkdir::WalkDir;
@@ -26,25 +27,32 @@ fn main() {
         println!("{}", entry.path().display());
 
         let file = ImageReader::open(entry.path());
-            if let Err(err) = file {
-                eprintln!("Failed to open {:?}: {}", entry.path(), err);
-                continue;
-            }
+        if let Err(err) = file {
+            eprintln!("Failed to open {:?}: {}", entry.path(), err);
+            continue;
+        }
         let content = file.unwrap();
         let img = content.decode();
-if let Err(err) = img {
-                eprintln!("Failed to decode {:?}: {}", entry.path(), err);
-                continue;
+        if let Err(err) = img {
+            eprintln!("Failed to decode {:?}: {}", entry.path(), err);
+            continue;
+        }
 
-}
-
-        let resized = img.unwrap().resize(18, 18, image::imageops::FilterType::Gaussian);
-        let grayscale = resized.grayscale();
+        let resized = img
+            .unwrap()
+            .resize(40, 40, image::imageops::FilterType::Nearest);
+        let mut grayscale = resized.grayscale().to_luma8();
+        grayscale
+            .pixels_mut()
+            .for_each(|p| *p = Luma::from(if p.0[0] > 255 / 2 { [1] } else { [0] }));
 
         let mut tmp_path = PathBuf::new();
         tmp_path.push("/tmp");
-        tmp_path.push(entry.path());
-        grayscale.save(&tmp_path).unwrap();
-        println!("Save {:?}", &tmp_path);
+        tmp_path.push(entry.file_name());
+        if let Err(err) = grayscale.save(&tmp_path) {
+            eprintln!("Failed to save {:?}: {}", &tmp_path, err);
+            continue;
+        }
+        println!("Saved {:?}", &tmp_path);
     }
 }
