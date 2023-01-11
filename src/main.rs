4f90299e7613bd51d4e401f1a1edca9093f0dbf2
Philippe Gaultier <philippe.gaultier@ppro.com>
2023-01-11 09:18:25 +0100

    resize+grayscal images

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use image::io::Reader as ImageReader;
 use std::ffi::OsStr;
-
+use std::path::PathBuf;
 use walkdir::WalkDir;
 
 fn main() {
-    let root = "/Users/pgaultier/";
+    let root = "/Users/pgaultier/Downloads";
     let known_extensions = [
         "png", "jpg", "gif", "bmp", "ico", "tiff", "webp", "avif", "pnm", "dds", "tga",
     ]
@@ -23,5 +24,27 @@ fn main() {
             continue;
         }
         println!("{}", entry.path().display());
+
+        let file = ImageReader::open(entry.path());
+            if let Err(err) = file {
+                eprintln!("Failed to open {:?}: {}", entry.path(), err);
+                continue;
+            }
+        let content = file.unwrap();
+        let img = content.decode();
+if let Err(err) = img {
+                eprintln!("Failed to decode {:?}: {}", entry.path(), err);
+                continue;
+
+}
+
+        let resized = img.unwrap().resize(18, 18, image::imageops::FilterType::Gaussian);
+        let grayscale = resized.grayscale();
+
+        let mut tmp_path = PathBuf::new();
+        tmp_path.push("/tmp");
+        tmp_path.push(entry.path());
+        grayscale.save(&tmp_path).unwrap();
+        println!("Save {:?}", &tmp_path);
     }
 }
