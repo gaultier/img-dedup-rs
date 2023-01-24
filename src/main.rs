98a1311c8f5f3423b16c27502d2fdf190f0e875e
Philippe Gaultier <philippe.gaultier@ppro.com>
2023-01-24 10:36:14 +0100

    maximize

--- src/main.rs
+++ src/main.rs
@@ -355,6 +355,7 @@ fn main() {
     let options = eframe::NativeOptions {
         drag_and_drop_support: false,
         initial_window_size: Some(egui::vec2(1600.0, 900.0)),
+        maximized: true,
         ..Default::default()
     };
     eframe::run_native(
